385faaff0d234d75ed4158d55c837de4b775a30a
AlexFoxJS <alexfoxjs@gmail.com>
2018-11-25 01:06:46 +0300

    master: 32 Using the Node Modules System
    Signed-off-by: AlexFoxJS <alexfoxjs@gmail.com>

--- app.js
+++ app.js
@@ -1,67 +1,9 @@
+// Imports
 const http = require("http");
-const fs = require("fs");
 
+const routes = require("./routes");
 
-const server = http.createServer((req, res) => {
-	const url = req.url;
-	const method = req.method;
-
-	console.log('[NODE Server] url -', url);
-
-	if (req.url === '/home') {
-		res.write(`
-			<html>
-				<header>
-					<title>NodeJS</title>
-				</header>
-				<body>
-					<form action="/message" method="POST">
-						<input type="text" name="message">
-						<button type="submit">Submit</button>
-					</form>
-				</body>
-			</html>
-		`);
-
-		return res.end();
-	}
-
-	if (url === '/message' && method === 'POST') {
-		const body = [];
-
-		req.on('data', chunk => {
-			console.log('[NODE Server] chunk -', chunk);
-			body.push(chunk);
-		});
-
-		return req.on('end', () => {
-			const parsedBody = Buffer.concat(body).toString();
-			const message = parsedBody.split('=')[1];
-
-			fs.writeFile('message.txt', message, err => {
-				res.statusCode = 302;
-				res.setHeader('Location', '/');
-
-				return res.end();
-			});
-
-		});
-
-	}
-
-	res.setHeader('Content-Type', 'text/html');
-	res.write(`
-		<html>
-			<header>
-				<title>First NodeJS title</title>
-			</header>
-			<body>
-				<h1>Hello Node.js Server!</h1>
-			</body>
-		</html>
-	`);
-	res.end();
-
-});
+// Code
+const server = http.createServer(routes);
 
 server.listen(3000);
---
+++ routes.js
@@ -0,0 +1,64 @@
+const fs = require('fs');
+
+const requestHandle = (req, res) => {
+	const url = req.url;
+	const method = req.method;
+
+	if (req.url === '/home') {
+		res.write(`
+			<html>
+				<header>
+					<title>NodeJS</title>
+				</header>
+				<body>
+					<form action="/message" method="POST">
+						<input type="text" name="message">
+						<button type="submit">Submit</button>
+					</form>
+				</body>
+			</html>
+		`);
+
+		return res.end();
+	}
+
+	if (url === '/message' && method === 'POST') {
+		const body = [];
+
+		req.on('data', chunk => {
+			console.log('[NODE Server] chunk -', chunk);
+			body.push(chunk);
+		});
+
+		return req.on('end', () => {
+			const parsedBody = Buffer.concat(body).toString();
+			const message = parsedBody.split('=')[1];
+
+			fs.writeFile('message.txt', message, err => {
+				res.statusCode = 302;
+				res.setHeader('Location', '/');
+
+				return res.end();
+			});
+
+		});
+
+	}
+
+	res.setHeader('Content-Type', 'text/html');
+	res.write(`
+		<html>
+			<header>
+				<title>First NodeJS title</title>
+			</header>
+			<body>
+				<h1>Hello Node.js Server!</h1>
+			</body>
+		</html>
+	`);
+	res.end();
+
+};
+
+
+module.exports = requestHandle;
