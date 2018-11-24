50371688d90bffcd8a8c9229bf9cdf0c3d4006bb
AlexFoxJS <alexfoxjs@gmail.com>
2018-11-24 23:16:41 +0300

    master: 25 Sending Responses
    Signed-off-by: AlexFoxJS <alexfoxjs@gmail.com>

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ const server = http.createServer((req, res) => {
 
 	console.log(url, method, headers);
 	// process.exit()
+
+	res.setHeader('Content-Type', 'text/html');
+	res.write(`
+		<html>
+			<header>
+				<title>First NodeJS title</title>
+			</header>
+			<body>
+				<h1>Hello Node.js Server!</h1>
+			</body>
+		</html>
+	`);
+	res.end();
 });
 
 server.listen(3000);
