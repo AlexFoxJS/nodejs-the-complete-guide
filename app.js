b91e548312160d64ae8126b4df9c387480a4921f
AlexFoxJS <alexfoxjs@gmail.com>
2018-11-24 23:10:47 +0300

    master: 24 Understanding Requests
    Signed-off-by: AlexFoxJS <alexfoxjs@gmail.com>

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-	console.log('req', req);
+	const { url, method, headers } = req;
+
+	console.log(url, method, headers);
+	// process.exit()
 });
 
 server.listen(3000);
