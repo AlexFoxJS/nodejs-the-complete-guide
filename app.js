ef94290b231769e911c9fcc77f76e7821892afdf
AlexFoxJS <alexfoxjs@gmail.com>
2018-11-29 00:08:59 +0300

    master: 53 Parsing Incoming Requests
    Signed-off-by: AlexFoxJS <alexfoxjs@gmail.com>

--- app.js
+++ app.js
@@ -3,18 +3,23 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-	console.log('3. This always runs!');
-	next();
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/add-product', (req, res, next) => {
-	console.log('2. Console log middleware!');
-	res.send('<h1>Hello "Add Product"</h1>');
+	res.send(`
+		<form action="/product" method="post">
+			<input type="text" name="title">
+			<button type="submit">Add Product</button>
+		</form>
+	`);
+});
+
+app.use('/product', (req, res, next) => {
+	console.log('4.', req.body);
+	res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-	console.log('1. Console log middleware!');
 	res.send('<h1>Hello from Express</h1>');
 });
 
