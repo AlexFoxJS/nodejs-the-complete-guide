ce5a1bab181a8000c489dd72320a731c5cd32988
AlexFoxJS <alexfoxjs@gmail.com>
2018-11-29 00:36:29 +0300

    master: 57 Filtering Paths
    Signed-off-by: AlexFoxJS <alexfoxjs@gmail.com>

--- routes/admin.js
+++ routes/admin.js
@@ -2,16 +2,18 @@ const express = require('express');
 
 const router = express.Router();
 
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
 	res.send(`
-		<form action="/product" method="post">
+		<form action="/admin/add-product" method="post">
 			<input type="text" name="title">
 			<button type="submit">Add Product</button>
 		</form>
 	`);
 });
 
-router.post('/product', (req, res, next) => {
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
 	console.log('4.', req.body);
 	res.redirect('/');
 });
