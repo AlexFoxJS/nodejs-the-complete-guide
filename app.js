55ef12edea96e3c02194af99a93464ac1dc5682e
AlexFoxJS <alexfoxjs@gmail.com>
2018-11-29 00:13:16 +0300

    master: 54 Limiting Middleware Execution to POST Requests
    Signed-off-by: AlexFoxJS <alexfoxjs@gmail.com>

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use('/add-product', (req, res, next) => {
 	`);
 });
 
-app.use('/product', (req, res, next) => {
+app.post('/product', (req, res, next) => {
 	console.log('4.', req.body);
 	res.redirect('/');
 });
