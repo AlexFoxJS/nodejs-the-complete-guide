9e6ec6508eccc49c52e2d1314457539831720e79
AlexFoxJS <alexfoxjs@gmail.com>
2018-11-28 23:48:03 +0300

    master: 52 Handling Different Routes
    Signed-off-by: AlexFoxJS <alexfoxjs@gmail.com>

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-const http = require('http');
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-	console.log('1. Console log middleware!');
-	next();
+app.use('/add-product', (req, res, next) => {
+	console.log('2. Console log middleware!');
+	res.send('<h1>Hello "Add Product"</h1>')
 });
 
-app.use((req, res, next) => {
-	console.log('2. Console log middleware!');
+app.use('/', (req, res, next) => {
+	console.log('1. Console log middleware!');
 	res.send('<h1>Hello from Express</h1>')
 });
 
